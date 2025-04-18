917c7cc2fc8e842ccd4c3821ecde1436f8475127
CopyypoC <william.wang650@gmail.com>
2025-04-18 03:45:45 -0700

    Add linked list

---
+++ linked-list.mjs
@@ -0,0 +1,88 @@
+export class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+    this.currNode = null;
+  }
+
+  append(value) {
+    const newTail = new Node(value);
+
+    if (this.headNode === null) {
+      this.headNode = newTail;
+      this.tailNode = newTail;
+    } else {
+      this.tailNode.nextNode = newTail;
+      this.tailNode = newTail;
+    }
+  }
+
+  prepend(value) {
+    const newHead = new Node(value);
+
+    if (this.headNode === null) {
+      this.headNode = newHead;
+      this.tailNode = newHead;
+    } else {
+      newHead.nextNode = this.headNode;
+      this.headNode = newHead;
+    }
+  }
+
+  size() {
+    let size = 0;
+    this.currNode = this.headNode;
+
+    while (this.currNode !== null) {
+      this.currNode = this.currNode.nextNode;
+      size++;
+    }
+    return size;
+  }
+
+  head() {
+    return this.headNode;
+  }
+
+  tail() {
+    return this.tailNode;
+  }
+
+  at(index) {
+    this.currNode = this.headNode;
+
+    for (let i = 0; i < index; i++) {
+      this.currNode = this.currNode.nextNode;
+    }
+    return this.currNode;
+  }
+
+  pop() {
+    this.tailNode = null;
+  }
+}
+
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+// LinkedList {
+//   head: Node1;
+//   tail: Node3;
+//   curr: Node2;
+//   Node1 {
+//     value,
+//     nextNode,
+//   }
+//   Node2 {
+//     value,
+//     nextNode,
+//   }
+//   Node3 {
+//     value,
+//     nextNode,
+//   }
+// }
---
+++ main.mjs
@@ -0,0 +1,12 @@
+import { LinkedList } from "./linked-list.mjs";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.prepend("cow");
+
+//  0      1      2
+// cow -> dog -> cat
+console.log("List.at(1):");
+console.log(list.at(1));
